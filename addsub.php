761959db0b0a9849764fc3f385fd2a5d1aa368eb
António Silva <antoniosilva1017@gmail.com>
2014-10-24 20:49:48 +0100

    Bug fix

--- addsub.php
+++ addsub.php
@@ -5,15 +5,6 @@ require_once 'class/Mailit.php';
 
 $data = parse_ini_file("config.ini", true);
 
-	set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
-		// error was suppressed with the @-operator
-		if (0 === error_reporting()) {
-			return false;
-		}
-
-		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-	});
-
 	$db = new DataBase(
 		$data['mysql']['hostname'], // localhost
 		$data['mysql']['database'], // DB name
@@ -29,7 +20,7 @@ $data = parse_ini_file("config.ini", true);
 	$mail = new Mailit("newsletter@example.com");
 
 	$msg = "Thanks for subscribing, hope you'll enjoy our newsletter";
-	$mail->sendMail($email, "Subscibe", $msg);
+	$mail->sendMail($email, "Subscribe", $msg);
 
 
 
